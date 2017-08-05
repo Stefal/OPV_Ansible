2578888a4bf18fb56abf3bd05b68ec15b791dc91
Benjamin BERNARD <benjamin.bernard@openpathview.fr>
2017-08-05 15:32:27 +0200

    Fixes some bugs #20

--- deployLXCdevContainers.sh
+++ deployLXCdevContainers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lxcExist=$( command -v lxc )
-sshKey=$( cat ~/.ssh/id_rsa.pub)
+lxcExist=`command -v lxc`
+sshKey=`cat ~/.ssh/id_rsa.pub`
 
 poil1Name="master"
 poil2Name="slave"
@@ -26,15 +26,15 @@ echo "Setup ssh"
 lxc exec $poil1Name -- bash -c "echo $sshKey > ~/.ssh/authorized_keys"
 lxc exec $poil2Name -- bash -c "echo $sshKey > ~/.ssh/authorized_keys"
 
-ipPoil1=$(lxc info $poil1Name | grep -P "eth0:\tinet\t" | awk '{print $3}')
-ipPoil2=$(lxc info $poil2Name | grep -P "eth0:\tinet\t" | awk '{print $3}')
+ipPoil1=`lxc info $poil1Name | grep -P "eth0:\tinet\t" | awk '{print $3}'`
+ipPoil2=`lxc info $poil2Name | grep -P "eth0:\tinet\t" | awk '{print $3}'`
 
 ssh root@$ipPoil1 -o StrictHostKeyChecking=no exit
 ssh root@$ipPoil2 -o StrictHostKeyChecking=no exit
 
 echo "Installing python on lxc container"
-lxc exec $poil1Name -- apt-get install python
-lxc exec $poil2Name -- apt-get install python
+lxc exec $poil1Name -- apt-get install python -y
+lxc exec $poil2Name -- apt-get install python -y
 
 echo "Finish !"
 echo "You are now ready to use this container with ansible"
